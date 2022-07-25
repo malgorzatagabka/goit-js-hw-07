05a3f673e7da123d5f6de1e1aa18f3ca138b3bcb
Małgorzata Gąbka <malgorzata.dycha@gmail.com>
2022-07-25 10:29:19 +0200

    task 1

---
+++ js/01-gallery.js
@@ -0,0 +1,49 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+const lightbox = document.querySelector(".gallery");
+const images = galleryItems
+  .map(
+    (image) =>
+      ` <div class="gallery__item">
+  <a class="gallery__link" href="${image.original}">
+    <img
+      class="gallery__image"
+      src="${image.preview}"
+      data-source="${image.original}"
+      alt="${image.description}"
+    />
+  </a>
+</div>`
+  )
+  .join("");
+
+lightbox.innerHTML = images;
+
+lightbox.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (e.target.nodeName !== "IMG") return;
+  const source = e.target.dataset.source;
+  const instance = basicLightbox.create(`
+     <img src="${source}" width="800" height="600">
+`);
+
+  instance.show();
+});
+
+// const onClick = (e) => {
+//   e.preventDefault();
+//   if (e.target.nodeName !== "IMG") return;
+
+//   const source = e.target.dataset.source;
+
+//   const instance = basicLightbox.create(`
+//     <img src="${source}" width="800" height="600">
+// `);
+
+//   instance.show();
+// };
+
+// lightbox.addEventListener("click", onClick);
+
+console.log(galleryItems);
